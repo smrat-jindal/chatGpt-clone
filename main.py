1cf6d014119fca0f57eea14dab180ece0205e796
Samrat Jindal <smrat.jindal@gmail.com>
2023-05-19 23:45:27 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, jsonify, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/api", methods= ("GET", "POST"))
+def qa():
+    if request.method == "POST":
+        data = {"result":"Hey"}
+        return  jsonify(data)
+    # return render_template("index.html")
+    data = {"result":"Hey"}
+    return  jsonify(data)
+app.run(debug=True)
