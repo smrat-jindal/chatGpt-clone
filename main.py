d228d2b692bbcd61b17b7b3e155d4f4893ab9a01
Samrat Jindal <smrat.jindal@gmail.com>
2023-06-01 22:23:40 +0530

    created env for file path api key & mongo url

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from flask import Flask, render_template, jsonify, request
 from flask_pymongo import PyMongo
- 
+from dotenv import load_dotenv
+import os
 import openai
 
-openai.api_key = "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"
+
+
+load_dotenv()
+
+openai.api_key = os.getenv("OPEN_AI_KEY")
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb+srv://smratjindal:QWERTyuiop%40123@pilot01.mj0qo7e.mongodb.net/chatGPT"
+app.config["MONGO_URI"] = os.getenv("MONGO_URL")
 mongo = PyMongo(app)
 
 @app.route("/")
